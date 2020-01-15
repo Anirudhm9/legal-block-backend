696ab0d462cb6be7c24fe01567300d9941015a6f
Anirudhm9 <anirudh.m0009@gmail.com>
2020-01-15 16:17:58 +1100

    Assign contract,notify by email and Contract Status

--- controllers/contractController/contractBaseController.js
+++ controllers/contractController/contractBaseController.js
@@ -6,6 +6,7 @@ var CodeGenerator = require("../../lib/codeGenerator");
 var ERROR = UniversalFunctions.CONFIG.APP_CONSTANTS.STATUS_MSG.ERROR;
 var _ = require("underscore");
 var Config = require("../../config");
+var NodeMailer = require('../../lib/nodeMailer');
 
 var createContract = function (userData, payloadData, callback) {
     var contract = null;
@@ -28,6 +29,7 @@ var createContract = function (userData, payloadData, callback) {
 
         function (cb) {
             payloadData.userId = userData._id
+            payloadData.contractStatus = Config.APP_CONSTANTS.DATABASE.CONTRACT_STATUS.PROCESSING;
             Service.ContractService.createContract(payloadData, function (err, data) {
                 if (err) cb(err)
                 else {
@@ -36,13 +38,83 @@ var createContract = function (userData, payloadData, callback) {
                 }
             })
         },
-
-
+        function (cb) {
+            var taskInParallel = [];
+            for (var key in payloadData.assignees) {
+                (function (key) {
+                    taskInParallel.push((function (key) {
+                        return function (embeddedCB) {
+                            Service.UserService.getUser({
+                                _id: payloadData.assignees[key]
+                            }, {}, {
+                                lean: true
+                            }, function (err, data) {
+                                if (err) {
+                                    embeddedCB(err)
+                                } else {
+                                    var user = data && data[0] || null;
+                                    console.log(user.emailId)
+                                    var name = user.firstName + ' ' + user.lastName;
+                                    NodeMailer.sendContractMail(user.emailId, name);
+                                    embeddedCB()
+                                }
+                            })
+                        }
+                    })(key))
+                }(key));
+            }
+            async.parallel(taskInParallel, function (err, result) {
+                cb(null);
+            });
+        }
     ], function (err, result) {
         if (err) callback(err)
         else callback(null, { data: contract })
     })
 }
+var viewAllContractsByCategory = function (userData, callback) {
+    var contracts = null;
+    async.series([
+
+        function (cb) {
+            var criteria = {
+                _id: userData._id,
+                userType: Config.APP_CONSTANTS.DATABASE.USER_ROLES.SUPERADMIN
+            };
+            Service.AdminService.getAdmin(criteria, { password: 0 }, {}, function (err, data) {
+                if (err) cb(err);
+                else {
+                    if (data.length == 0) cb(ERROR.INCORRECT_ACCESSTOKEN);
+                    else {
+                        userFound = (data && data[0]) || null;
+                        cb();
+                    }
+                }
+            });
+        },
+        function (cb) {
+            criteria = [
+                {
+                    $group: {
+                        _id: '$contractType',
+                        contracts: { $push: "$$ROOT" }
+                    },
+                },
+            ]
+            Service.ContractService.getAggregateContracts(criteria, function (err, data) {
+                if (err) cb(err)
+                else {
+                    contracts = data;
+                    cb();
+                }
+            })
+        },
+    ], function (err, result) {
+        if (err) callback(err)
+        else callback(null, { data: contracts })
+    })
+}
+
 
 var getContractsbyCategory = function (userData, callback) {
     var contracts = null;
@@ -112,11 +184,14 @@ var updateContract = function (userData, payloadData, callback) {
 
         function (cb) {
             var dataToSet = {
-                contractName: payloadData.contractName,
-                contractType: payloadData.contractType,
-                content: payloadData.content,
-                clientName: payloadData.clientName,
-                dateAssigned: Date.now()
+                $set: {
+                    contractName: payloadData.contractName,
+                    contractType: payloadData.contractType,
+                    content: payloadData.content,
+                    clientName: payloadData.clientName,
+                    assignees: payloadData.assignees,
+                    dateAssigned: Date.now()
+                }
             }
             Service.ContractService.updateContracts({ _id: payloadData.contractId, userId: userData._id }, dataToSet, {}, function (err, data) {
                 if (err) cb(err)
@@ -174,5 +249,6 @@ module.exports = {
     createContract: createContract,
     getContractsbyCategory: getContractsbyCategory,
     updateContract: updateContract,
-    deleteContract: deleteContract
+    deleteContract: deleteContract,
+    viewAllContractsByCategory: viewAllContractsByCategory
 };
