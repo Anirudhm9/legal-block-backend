6abe0ce17ee7a82ea2a519af33cbb8ac48d2e6ae
Anirudhm9 <anirudh.m0009@gmail.com>
2020-01-28 15:34:49 +1100

    file name change

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,6 @@ var UserBaseRoute = require("./userRoute/userBaseRoute");
 var AdminBaseRoute = require("./adminRoute/adminBaseRoute");
 var TemplateBaseRoute = require("./templateLibraryRoute/templateLibraryBaseRoute");
 var ContractBaseRoute = require("./contractRoute/contractBaseRoute");
-var RequestBaseRoute = require("./requestRoute/requesetBaseRoute");
+var RequestBaseRoute = require("./requestRoute/requestBaseRoute");
 var APIs = [].concat(DemoBaseRoute, UserBaseRoute, AdminBaseRoute, TemplateBaseRoute, ContractBaseRoute, RequestBaseRoute);
 module.exports = APIs;
